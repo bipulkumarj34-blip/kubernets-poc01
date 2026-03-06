6d1f0e4c4768f902086616ae7c585960282b7bbd
bjha <bjha@nisum.com>
2026-03-06 15:40:55 +0530

    syntax fixed

--- main.tf
+++ main.tf
@@ -3,40 +3,36 @@ provider "google" {
   region  = "us-central1"
 }
 
-# 1. GKE Cluster Definition
 resource "google_container_cluster" "primary" {
   name               = "poc-cluster"
   location           = "us-central1-a"
   initial_node_count = 1
-  
   workload_identity_config {
     workload_pool = "devops-489010.svc.id.goog"
   }
 }
 
-# 2. Kubernetes Provider Configuration
-# This tells Terraform how to connect to the GKE cluster created above
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "gke-gcloud-auth-plugin" # Uses the plugin we installed
+    command     = "gke-gcloud-auth-plugin"
   }
 }
 
-# 3. ConfigMap
-resource "kubernetes_config_map" "app_config" {
+# Updated to _v1 to remove warnings
+resource "kubernetes_config_map_v1" "app_config" {
   metadata {
     name = "app-config"
   }
   data = {
-    "APP_COLOR" = "blue" 
+    "APP_COLOR" = "blue"
   }
 }
 
-# 4. Secret
-resource "kubernetes_secret" "db_pass" {
+# Updated to _v1
+resource "kubernetes_secret_v1" "db_pass" {
   metadata {
     name = "db-pass"
   }
@@ -45,8 +41,8 @@ resource "kubernetes_secret" "db_pass" {
   }
 }
 
-# 5. Deployment (Fixed Syntax)
-resource "kubernetes_deployment" "app" {
+# Updated to _v1
+resource "kubernetes_deployment_v1" "app" {
   metadata {
     name = "config-poc"
   }
@@ -63,28 +59,23 @@ resource "kubernetes_deployment" "app" {
           app = "poc"
         }
         annotations = {
-          # This forces a rollout if you change the ConfigMap data
-          "checksum/config" = sha256(jsonencode(kubernetes_config_map.app_config.data))
+          "checksum/config" = sha256(jsonencode(kubernetes_config_map_v1.app_config.data))
         }
       }
       spec {
         container {
           name  = "webapp"
           image = "nginx:latest"
-
-          # Fixed: Multi-line block for ConfigMap Injection
           env_from {
             config_map_ref {
-              name = kubernetes_config_map.app_config.metadata[0].name
+              name = kubernetes_config_map_v1.app_config.metadata[0].name
             }
           }
-
-          # Fixed: Multi-line block for Secret Injection
           env {
             name = "DB_PASSWORD"
             value_from {
               secret_key_ref {
-                name = kubernetes_secret.db_pass.metadata[0].name
+                name = kubernetes_secret_v1.db_pass.metadata[0].name
                 key  = "password"
               }
             }
